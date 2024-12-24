87d21f1dc9f59238c77aad25cc9719854ab8cbfe
chadda <chadha.essid@insat.ucar.tn>
2024-12-24 17:15:12 +0100

    feature: configure routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {IngredientComponent} from "./ingredient/ingredient.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'ingredients',component:IngredientComponent},
+];
