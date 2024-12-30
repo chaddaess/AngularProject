910198f21113459f807b14df6838cf4fc98bbd59
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2024-12-30 21:09:49 +0100

    Removed unused directive

--- src/app/muscle-tooltip/muscle-tooltip.component.ts
+++ src/app/muscle-tooltip/muscle-tooltip.component.ts
@@ -1,14 +1,11 @@
 import {Component, Input} from '@angular/core';
 import {Muscle} from "../model/muscle";
 import {API} from "../../config/api.config";
-import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-muscle-tooltip',
   standalone: true,
-  imports: [
-    NgOptimizedImage
-  ],
+  imports: [],
   templateUrl: './muscle-tooltip.component.html',
   styleUrl: './muscle-tooltip.component.css'
 })
