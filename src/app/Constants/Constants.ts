bc93074ef42c0c37901829a50a471f0bb92a8860
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2025-01-11 16:41:36 +0100

    routes & some constants

--- src/app/Constants/Constants.ts
+++ src/app/Constants/Constants.ts
@@ -12,7 +12,6 @@ export const UI_TEXTS = {
   EMAIL_INVALID_ERROR: "Enter a valid email.",
   PASSWORD_LABEL: "Password",
   PASSWORD_REQUIRED_ERROR: "Password is required.",
-  FORGOT_PASSWORD_PROMPT: "Forgot password?",
   USERNAME_LABEL: "Username",
   USERNAME_REQUIRED_ERROR: "Username is required.",
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {AppComponent} from "./app.component";
+import {HttpClientModule} from "@angular/common/http";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  { path: '', component: AppComponent },
+
+];
