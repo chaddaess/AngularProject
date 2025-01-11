ebaee61a520679231afe1760d1f4ec5fa04ff740
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2025-01-11 16:15:24 +0100

    some constants

---
+++ src/app/Constants/Constants.ts
@@ -0,0 +1,16 @@
+export const API_BASE_URL = "https://wger.de";
+export const LOGIN_URL = `${API_BASE_URL}/api/v2/login/`;
+export const REGISTER_URL = `${API_BASE_URL}/api/v2/register/`;
+
+export const UI_TEXTS = {
+  APP_TITLE: "HealthSphere",
+  SIGN_IN_TITLE: "Sign in to your account",
+  SIGN_UP_TITLE: "Sign Up",
+  CREATE_ACCOUNT_PROMPT: "Don’t have an account yet?",
+  EMAIL_LABEL: "Your Email",
+  EMAIL_REQUIRED_ERROR: "Email is required.",
+  EMAIL_INVALID_ERROR: "Enter a valid email.",
+  PASSWORD_LABEL: "Password",
+  PASSWORD_REQUIRED_ERROR: "Password is required.",
+  FORGOT_PASSWORD_PROMPT: "Forgot password?"
+};
