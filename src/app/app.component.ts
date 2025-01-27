9ee1d51524d3ee064037c29ada8799ffb5263e5a
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2025-01-27 23:21:29 +0100

    add some error & success messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {LoginComponent} from "./login/login.component";
 import {Component, inject} from '@angular/core';
 import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet} from '@angular/router';
 import {LoaderComponent} from "./loader-page/loader.component";
+import {APP_TITLE} from "../config/const.config";
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,7 @@ import {LoaderComponent} from "./loader-page/loader.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title : string = "HealthSphere"
+  title : string = APP_TITLE;
   isLoading = false;
   router=inject(Router)
   constructor() {
