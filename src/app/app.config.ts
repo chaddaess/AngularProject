09464b54052b6ac1a9ec4ba5f43e6b7a4b70a103
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2025-01-28 23:11:26 +0100

    add interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient} from "@angular/common/http";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {provideAnimations} from "@angular/platform-browser/animations";
 import {provideToastr} from "ngx-toastr";
+import {authInterceptor} from "./auth-interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(),provideRouter(routes),provideAnimations(),
+  providers: [provideHttpClient(
+    withInterceptors([authInterceptor]),
+  ),provideRouter(routes),provideAnimations(),
     provideToastr({
       positionClass: 'toast-top-right',
       timeOut: 5000,
---
+++ src/app/auth-interceptor.ts
@@ -0,0 +1,16 @@
+import { HttpRequest, HttpHandlerFn, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export function authInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+  const token = localStorage.getItem('accessToken');
+console.log(token)
+  if (token) {
+    const authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(authReq);
+  }
+  return next(req);
+}
